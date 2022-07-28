90c83c51fc7410f8680772c7883e77066990015d
Joydwipdatta <109138173+Joydwipdatta@users.noreply.github.com>
2022-07-28 17:53:36 +0530

    Add files via upload

---
+++ diplay/conn.php
@@ -0,0 +1,12 @@
+<?php
+$connection= new mysqli('localhost','root','','newlogin'); 
+if(!$connection){ 
+    die(mysqli_error($connection));
+  // echo  "conn uccessful"  ;    
+   
+//}else{
+  // echo "nont done";
+}
+?>
+        
+    
